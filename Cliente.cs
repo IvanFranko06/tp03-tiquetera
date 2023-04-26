c48be401a8d24de1113a60ec5125b71db9f3395a
IvanFranko06 <frankoivan06@gmail.com>
2023-04-26 20:56:03 -0300

    create new console project

---
+++ Cliente.cs
@@ -0,0 +1,37 @@
+
+namespace tp3
+{
+    class Cliente
+    {
+
+        public string Nombre { get; private set; }
+        public int DNI { get; private set; }
+        public int TipoEntrada { get; set; }
+
+        public int TotalAbonado { get; set; }
+
+        public DateTime FechaInscripcion { get; set; }
+        public string Apellido { get; private set; }
+
+        public Cliente()
+        {
+         Nombre ="";
+         Apellido = "";
+         DNI=0;
+         TipoEntrada=0;
+         FechaInscripcion=new DateTime();
+        }
+        public Cliente(string nom, string ape, int dni, int tipoE,int totalA, DateTime nacimiento)
+        {
+            Nombre = nom;
+            DNI = dni;
+            TipoEntrada = tipoE;
+            FechaInscripcion = nacimiento;
+            Apellido = ape;
+            TotalAbonado = totalA;
+        }
+
+
+        
+    }
+}
